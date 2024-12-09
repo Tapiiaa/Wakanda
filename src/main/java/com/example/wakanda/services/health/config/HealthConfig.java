dd8a7cb3ccdedb4702256bbd7b2120a18b747cee
Tapiiaa <ptapilob@myuax.com>
2024-12-09 10:34:08 +0100

    paquete config

--- src/main/java/com/example/wakanda/services/health/config/HealthConfig.java
+++ src/main/java/com/example/wakanda/services/health/config/HealthConfig.java
@@ -1,4 +1,15 @@
 package com.example.wakanda.services.health.config;
 
+import org.springframework.context.annotation.Configuration;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+@Configuration
 public class HealthConfig {
+
+    private static final int THREAD_POOL_SIZE = 5;
+
+    public ExecutorService taskExecutor() {
+        return Executors.newFixedThreadPool(THREAD_POOL_SIZE);
+    }
 }
