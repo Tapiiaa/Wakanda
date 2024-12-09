47a56cac554a826f41cebadcf06f00b938cf8768
itsedupablo <pabloedufontes13@gmail.com>
2024-12-09 11:29:19 +0100

    Eliminación de constructores, getters y setters
    Está configurado Lombok y no me había dado cuenta

--- src/main/java/com/example/wakanda/services/traffic/model/PublicTransportRoute.java
+++ src/main/java/com/example/wakanda/services/traffic/model/PublicTransportRoute.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import lombok.Data;
 
 @Entity
+@Data
 public class PublicTransportRoute {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -16,69 +17,6 @@ public class PublicTransportRoute {
     private String ultimaActualizacion;
     private String siguienteParada;
     
-    public PublicTransportRoute() {}
-    
-	public PublicTransportRoute(Long id, String nombreRuta, String estadoActual, String tiempoLlegada,
-    		int desviosActivos, String ultimaActualizacion, String siguienteParada) {
-		
-	}
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getNombreRuta() {
-		return nombreRuta;
-	}
-
-	public void setNombreRuta(String nombreRuta) {
-		this.nombreRuta = nombreRuta;
-	}
-
-	public String getEstadoActual() {
-		return estadoActual;
-	}
-
-	public void setEstadoActual(String estadoActual) {
-		this.estadoActual = estadoActual;
-	}
-
-	public String getTiempoLlegada() {
-		return tiempoLlegada;
-	}
-
-	public void setTiempoLlegada(String tiempoLlegada) {
-		this.tiempoLlegada = tiempoLlegada;
-	}
-
-	public String getDesviosActivos() {
-		return desviosActivos;
-	}
-
-	public void setDesviosActivos(String desviosActivos) {
-		this.desviosActivos = desviosActivos;
-	}
-
-	public String getUltimaActualizacion() {
-		return ultimaActualizacion;
-	}
-
-	public void setUltimaActualizacion(String ultimaActualizacion) {
-		this.ultimaActualizacion = ultimaActualizacion;
-	}
-
-	public String getSiguienteParada() {
-		return siguienteParada;
-	}
-
-	public void setSiguienteParada(String siguienteParada) {
-		this.siguienteParada = siguienteParada;
-	}
-	
-	
+ 
 }
 
