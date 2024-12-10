065cbf08ddc013b0cde88e4391fb19edd37a76c1
Patrik <psirb@myuax.com>
2024-12-10 17:30:01 +0100

    HealthController. Falta crear toda la logica de detras.

--- src/main/java/com/example/wakanda/services/health/controller/HealthController.java
+++ src/main/java/com/example/wakanda/services/health/controller/HealthController.java
@@ -1,4 +1,23 @@
 package com.example.wakanda.services.health.controller;
 
+import com.example.wakanda.services.health.service.HealthService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/health")
 public class HealthController {
+
+    private final HealthService healthService;
+
+    public HealthController(HealthService healthService) {
+        this.healthService = healthService;
+    }
+
+    // Endpoint para obtener todos los datos de salud
+    @RequestMapping("/all")
+    public String getAllHealthMonitors() {
+        // Falta crear el resto de codigo para implementar la logica.
+        return "";
+    }
 }
