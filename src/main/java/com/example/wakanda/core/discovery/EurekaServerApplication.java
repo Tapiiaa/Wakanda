3b136f15d65439bc6c305ec3887ca1f146853833
itsedupablo <pabloedufontes13@gmail.com>
2024-12-17 18:25:18 +0100

    Solucionado error Eureka Server (inyección dependencias correctas)

--- src/main/java/com/example/wakanda/core/discovery/EurekaServerApplication.java
+++ src/main/java/com/example/wakanda/core/discovery/EurekaServerApplication.java
@@ -5,10 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableEurekaServer // Habilita el Eureka Server
+@EnableEurekaServer
 public class EurekaServerApplication {
-
     public static void main(String[] args) {
-        SpringApplication.run(EurekaServerApplication.class, args); // Arranca el Eureka Server
+        SpringApplication.run(EurekaServerApplication.class, args);
     }
 }
