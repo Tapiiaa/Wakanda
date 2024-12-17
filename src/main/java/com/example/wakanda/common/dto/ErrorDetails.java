8bfa7901feb2fc9444f3f67d6bc12b4282dae92e
Tapiiaa <ptapilob@myuax.com>
2024-12-17 22:47:03 +0100

    dto

--- src/main/java/com/example/wakanda/common/dto/ErrorDetails.java
+++ src/main/java/com/example/wakanda/common/dto/ErrorDetails.java
@@ -1,5 +1,6 @@
 package com.example.wakanda.common.dto;
 
+
 import java.util.Date;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -12,4 +13,14 @@ public class ErrorDetails {
     private Date timestamp;
     private String message;
     private String details;
+
+    public String formatError() {
+        return "[" + timestamp + "] " + message + ": " + details;
+    }
+
+    public void updateError(String newMessage, String newDetails) {
+        this.message = newMessage;
+        this.details = newDetails;
+        this.timestamp = new Date();
+    }
 }
