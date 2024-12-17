30b5f6dcb27fe298b73017c3b86b6f39779bde67
paatriksirbu <psirb@myuax.es>
2024-12-17 19:20:46 +0100

    Metodo getAllHealthMonitors.

--- src/main/java/com/example/wakanda/services/health/controller/HealthController.java
+++ src/main/java/com/example/wakanda/services/health/controller/HealthController.java
@@ -1,8 +1,12 @@
 package com.example.wakanda.services.health.controller;
 
+import com.example.wakanda.services.health.model.HealthMonitor;
 import com.example.wakanda.services.health.service.HealthService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/health")
@@ -16,9 +20,8 @@ public class HealthController {
 
     // Endpoint para obtener todos los datos de salud
     @RequestMapping("/all")
-    public String getAllHealthMonitors() {
-        // Falta crear el resto de codigo para implementar la logica.
-        return "";
+    public ResponseEntity<List<HealthMonitor>> getAllHealthMonitors() {
+        return ResponseEntity.ok(healthService.getAllHealthMonitors());
     }
 }
 
